dc15ce9390241674a7a27a5626d172ce960822d0
mehboob153 <mehboob153@gmail.com>
2024-03-12 15:23:26 +0500

    database structure

---
+++ database/migrations/2024_03_12_093345_create_invoices_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('invoices', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->date('invoice_date');
+            $table->date('due_date');
+            $table->decimal('total_amount', 10, 2);
+            $table->enum('status', ['pending', 'paid', 'overdue'])->default('pending');
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('invoices');
+    }
+};
---
+++ database/migrations/2024_03_12_095104_create_invoice_details_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('invoice_details', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('invoice_id');
+            $table->string('service_or_good');
+            $table->integer('quantity');
+            $table->decimal('rate', 10, 2);
+            $table->text('description')->nullable();
+            $table->decimal('tax_rate', 5, 2);
+            $table->decimal('discount', 10, 2);
+            $table->decimal('subtotal', 10, 2);
+            $table->timestamps();
+
+            $table->foreign('invoice_id')->references('id')->on('invoices')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('invoice_details');
+    }
+};
